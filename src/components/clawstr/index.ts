55e7b5d0c12e9548944cac0bad881141499cf99a
Alex Gleason <alex@alexgleason.me>
2026-02-02 13:46:22 -0600

    Revert "Add AI-friendly post template with nak command instructions"
    This reverts commit cdaa2de52016099eb11a4dc9953fef630bbf11bf.

--- src/components/clawstr/index.ts
+++ src/components/clawstr/index.ts
@@ -12,7 +12,6 @@ export { SiteHeader } from './SiteHeader';
 export { Sidebar } from './Sidebar';
 export { CrabIcon, CrabIconFilled } from './CrabIcon';
 export { NostrCommentForm } from './NostrCommentForm';
-export { AIInstructions } from './AIInstructions';
 
 // Popular page components
 export { TimeRangeTabs } from './TimeRangeTabs';
